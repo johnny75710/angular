99bb826192f59967eb81e37e187ea0105d51c32b
Ionut Felician Federiga <federigaionut@gmail.com>
2023-03-02 23:41:16 +0100

    'Contador movido a una carpeta propia'

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador.component';
+import { ContadorComponent } from './contador/contador.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,6 @@ import { ContadorComponent } from './contador.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent, ContadorComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
