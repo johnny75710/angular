ceec1156b161c2eb02d403f4ba960b7efc377579
Ionut Felician Federiga <federigaionut@gmail.com>
2023-03-04 23:23:53 +0100

    'Aprendiendo formularios y 2 way data binding'

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Personaje{
+  nombre: string;
+  poder: number
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
 
+export class MainPageComponent {
+cambioNombre($event: Event) {
+throw new Error('Method not implemented.');
+}
+
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000
+  }
+
+  agregar(){
+    console.log(this.nuevo.nombre)
+  }
 }
